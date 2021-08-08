d6280715fa65d268d94be22b72253fe01d16dbd5
Flippie Coetser <Flippie.Coetser@gmail.com>
2021-08-08 12:14:47 -0700

    Implement Base Component

--- lib/Component.d.ts
+++ lib/Component.d.ts
@@ -1,3 +1,16 @@
 export declare class Component extends HTMLElement {
+    static get observedAttributes(): string[];
+    static attributes: string[];
+    connected: boolean;
+    root: ShadowRoot;
+    template: HTMLTemplateElement;
     constructor();
+    get attribute(): string;
+    set attribute(value: string);
+    connectedCallback(): void;
+    disconnectedCallback(): void;
+    adoptedCallback(): void;
+    attributeChangedCallback(attribute: string, oldValue: string, newValue: string): void;
+    loadTemplate(): void;
+    addTemplate(): void;
 }
--- src/Component.ts
+++ src/Component.ts
@@ -1,13 +1,75 @@
 export class Component extends HTMLElement {
-    constructor() {
+    static get observedAttributes(): string[] {
+        return Component.attributes;
+    }
+
+    static attributes: string[] = ['attribute'];
+
+    public connected: boolean = false;
+    public root: ShadowRoot;
+    public template: HTMLTemplateElement;
+
+    constructor() { 
         super();
+        this.root = this.attachShadow({mode: 'closed'});
+        this.addTemplate();
+    }
 
-        // https://stackoverflow.com/questions/33749235/can-one-include-templates-in-a-html-file-similar-to-css
-        // https://www.html5rocks.com/en/tutorials/webcomponents/imports/
-        const template = <HTMLTemplateElement>document.getElementById('CustomTemplate');
-        const clone = template.content.cloneNode(true)
+    get attribute(): string {
+        return this.getAttribute('attribute');
+    }
 
-        this.attachShadow({mode: 'open'});
-        this.shadowRoot.appendChild(clone);
+    set attribute(value: string) {
+        this.setAttribute('attribute', value);
+    }
+
+    public connectedCallback(): void {
+        console.log('Component Connected to DOM');
+        this.connected = true;
+    }
+
+    public disconnectedCallback(): void {
+        console.log('Component Disconnected from DOM');
+        this.connected = false;
+    }
+
+    public adoptedCallback(): void {
+        console.log('Component Move to new DOM');
+    }
+
+    public attributeChangedCallback(attribute: string, 
+                                    oldValue: string, 
+                                    newValue: string): void {
+        
+        if (this.connected) { 
+            console.log('Attribute Changed After Component Connected to DOM')
+        };
+        if (!this.connected) { 
+            console.log('Attribute Defined Before Component Connected to DOM')
+        };
+
+        if (oldValue === newValue) { 
+            return 
+        }
+
+        switch (attribute) {
+            case 'attribute':
+                console.log(`Attribute Value Changed from ${oldValue} to ${newValue}`)
+        }
+    }
+
+    public loadTemplate() {
+        this.template = document.createElement('template')
+        this.template.innerHTML = `
+        <head>
+            <link rel="stylesheet" href="../lib/component.style.css">
+        </head>
+        <div id="panelId"></div>
+        `
+    }
+
+    public addTemplate() {
+        this.loadTemplate()
+        this.root.appendChild(this.template.content.cloneNode(true));
     }
 }
