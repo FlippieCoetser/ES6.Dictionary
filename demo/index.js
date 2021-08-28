59d6a6c4ee4ef8dbcfe229d41f1fe43625165b91
Flippie Coetser <Flippie.Coetser@gmail.com>
2021-08-28 03:52:36 -0700

    Check

--- demo/index.js
+++ demo/index.js
@@ -1,7 +1,7 @@
 import { Dictionary } from '../lib/Dictionary.js'
 
 let dictionary = new Dictionary();
-dictionary.add('key','value');
+dictionary.add('key','value stored in dictionary');
 let value = dictionary.get('key');
 
 console.log(value)
